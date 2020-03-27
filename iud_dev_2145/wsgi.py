e0bca597754ab17bc263ce46fd84eb43431edd1b
Crowdbotics <team@crowdbotics.com>
2020-03-27 13:37:35 +0000

    Update models

--- iud_dev_2145/wsgi.py
+++ iud_dev_2145/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iud_dev_2145.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iud_dev_2145.settings")
 
 application = get_wsgi_application()
